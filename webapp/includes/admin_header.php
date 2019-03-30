146764d0ea464e4265fd7e1204b65eb98dfac236
Shashank Pandey <pandeyshashank152@gmail.com>
2019-03-31 00:55:04 +0530

    css includes fonts

---
+++ webapp/includes/admin_header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link href="css/material-icons.css" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link type="text/css" rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Gupta Jewellers</title>
+</head>
+
+<body>
+
+  <nav class="blue darken-2">
+    <div class="container">
+      <div class="nav-wrapper">
+
+        <a href="index.php" class="brand-logo">
+          GuptaJewellers
+        </a>
+
+        <a href="" data-activates="side-nav" class="button-collapse show-on-large right">
+          <i class="material-icons">menu</i>
+        </a>
+        <ul class="right hide-on-med-and-down">
+
+
+          <li>
+            <a href="admin.php">Dashboard</a>
+          </li>
+
+          <li>
+            <a href="admin_collateral.php">Collateral</a>
+          </li>
+          
+          <li>
+            <a href="deleted.php">Deleted/Paid Entries</a>
+          </li>
+          
+        </ul>
+        <ul id="side-nav" class="side-nav">
+          <li>
+            <div class="user-view">
+              <div class="background">
+                <img src="img/ocean.jpg" alt="">
+              </div>
+              <a href="">
+                <img src="img/logo.png" alt="" class="circle">
+              </a>
+              <a href="#">
+                <span class="name white-text">Gupta Jewellers</span>
+              </a>
+              <a href="#">
+                <span class="email white-text">gupta_jewellers@gmail.com</span>
+              </a>
+            </div>
+          </li>
+          
+          <li>
+            <a href="admin.php">Dashboard</a>
+          </li>
+
+          <li>
+            <a href="admin_collateral.php">Collateral</a>
+          </li>
+          
+          <li>
+            <a href="deleted.php">Deleted/Paid Entries</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ webapp/includes/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link href="css/material-icons.css" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link type="text/css" rel="stylesheet" href="css/main.css" />
+  <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Save.AI</title>
+</head>
+
+<body>
+
+  <nav class="blue darken-2">
+    <div class="container">
+      <div class="nav-wrapper">
+
+        <a href="index.php" class="brand-logo">
+          Save.AI
+        </a>
+
+        <a href="" data-activates="side-nav" class="button-collapse show-on-large right">
+          <i class="material-icons">menu</i>
+        </a>
+        <ul class="right hide-on-med-and-down">
+
+
+          <li>
+            <a href="index.php">Dashboard</a>
+          </li>
+
+          <li>
+            <a href="collateral.php">Twitter</a>
+          </li>
+          <li>
+            <a href="pawn.php">Facebook</a>
+          </li>
+          <li>
+            <a href="repay.php">Reddit</a>
+          </li>
+          
+        </ul>
+        <ul id="side-nav" class="side-nav">
+          
+          
+          <li >
+            <a href="index.php">Dashboard</a>
+          </li>
+
+          <li>
+            <a href="collateral.php">Twitter</a>
+          </li>
+          <li>
+            <a href="pawn.php">Facebook</a>
+          </li>
+          <li>
+            <a href="repay.php">Reddit</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
